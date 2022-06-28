04a93dc71b8dbe48b9d5408dd1213a18d23c1a65
BespalovVV <108338444+BespalovVV@users.noreply.github.com>
2022-06-28 15:56:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,11 @@ def write_in_txt_file():
     with open('test.txt', 'w') as writer:
         writer.write('I love Git')
         return True
+if __name__ == "__main__":
+	if write_in_txt_file():
+		print("[OK]write")
+		
+	print(read_txt_file())
+	
+	if delete_file():
+		print("[OK]delete")
