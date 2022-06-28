19a660995107282e3c154d9a88fabf61fa1cdca4
BespalovVV <108338444+BespalovVV@users.noreply.github.com>
2022-06-28 16:39:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def read_txt_file():
         
 # Bespalov : пишет в файл
 def write_in_txt_file():
-    with open('test.txt', 'w') as writer:
+    with open('file.txt', 'w') as writer:
         writer.write('I love Git')
         return True
 
